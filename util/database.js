5e1bfc249a1628abaec316b6eb22b143a7333e6f
rafaelqsistemas <rafaelq.sistemas@gmail.com>
2019-01-27 19:19:54 -0400

    copied backend template and modified dependencies

---
+++ util/database.js
@@ -0,0 +1,9 @@
+ const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('helixflights', 'helixadmin', '123456helix', {
+  dialect: 'mysql',
+  host: 'aethersol.com',
+  port: 3306
+});
+
+module.exports = sequelize;
